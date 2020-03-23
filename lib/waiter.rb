372dd5fa4014378cf8cdd37f66fee203c1128897
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-23 14:44:44 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,22 @@
 class Waiter
 
+attr_accessor :name, :yrs_experience
+  
+  @@all = [ ]
+  
+  def initialize(name, age)
+    @name = name 
+    @yrs_experience = yrs_experience
+    @@all << self
+  end 
+  
+  def self.all
+    @@all
+  end 
+
+  def waiters
+    Meals.all.select do |meal| 
+      meal.waiter == self
+    end
+  end 
 end
